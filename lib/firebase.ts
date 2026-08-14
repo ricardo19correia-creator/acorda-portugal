a3e6a5cd4f3eb42b47b9a3b52e327b72feb53f18
ricardo19correia-creator <ricardo19correia@gmail.com>
2026-08-14 14:39:18 -0200

    Update Firebase web configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDMARrDXtL6-AamKwLSXyu34iIZ6KuR-WM',
+  apiKey: 'AIzaSyAitsm_neLuW95B5spzFIyjzhJWUeF3FzE',
   authDomain: 'desafio-nacional-5fe71.firebaseapp.com',
   projectId: 'desafio-nacional-5fe71',
-  storageBucket: 'desafio-nacional-5fe71.appspot.com',
-  messagingSenderId: '789316383907',
-  appId: '1:789316383907:web:3b597f5edafd2cd1f73b16',
+  storageBucket: 'desafio-nacional-5fe71.firebasestorage.app',
+  messagingSenderId: '130539395859',
+  appId: '1:130539395859:web:e3b8153477ae41d6fe98e6',
 }
 
 const app = getApps().length > 0
