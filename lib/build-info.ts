b59bf5f2d994e4944b9c6ca996969b9c3cd6ac40
ricardo19correia-creator <ricardo19correia@gmail.com>
2026-08-31 03:59:03 -0200

    chore: update build info commit hash

--- lib/build-info.ts
+++ lib/build-info.ts
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   version: '1.0.0-rc.1',
-  commit: 'd6e786e',
-  buildDate: '2026-08-31T05:32:00Z',
+  commit: '37375d6',
+  buildDate: '2026-08-31T05:58:00Z',
   target: 'production',
   platform: 'hybrid-web-android',
   firebaseProject: 'desafio-nacional-5fe71',
