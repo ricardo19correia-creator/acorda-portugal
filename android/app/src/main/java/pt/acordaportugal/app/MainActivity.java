2890423e3d896d78da612d566740d21db2b05799
ricardo19correia-creator <ricardo19correia@gmail.com>
2026-09-01 02:55:43 -0200

    release: Acorda Portugal v1.0.0-rc.8 (VersionCode: 8) - Official Release Candidate with standard native BridgeActivity and clean deep linking

--- android/app/src/main/java/pt/acordaportugal/app/MainActivity.java
+++ android/app/src/main/java/pt/acordaportugal/app/MainActivity.java
@@ -1,20 +1,5 @@
 package pt.acordaportugal.app;
 
-import android.os.Bundle;
-import android.webkit.WebStorage;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try {
-            WebView webView = getBridge().getWebView();
-            if (webView != null) {
-                webView.clearCache(true);
-            }
-            WebStorage.getInstance().deleteAllData();
-        } catch (Exception ignored) {}
-    }
-}
+public class MainActivity extends BridgeActivity {}
